45f99c294327d52028eeb9c7371ee7d04af52021
Grant Wu <grantwu2@gmail.com>
2015-01-25 23:44:44 -0800

    Implement KML parsing via toGeoJSON

--- static/app.js
+++ static/app.js
@@ -1,21 +1,9 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var latlng = L.latLng(37.7833, -122.4167);
+var map = L.map('map').setView(latlng, 13);
 
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/grantbot.kjfg3cap/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18
     }).addTo(map);
 
-var popup = L.popup();
-
-function onMapClick(e) {
-  console.log(e);
-  popup
-    .setLatLng(e.latlng)
-    .setContent("You clicked the map at " + e.latlng.toString())
-    .openOn(map);
-}
-
-map.on('click', onMapClick);
-
-
-
+omnivore.kml('static/history-12-31-1969.kml').addTo(map);
