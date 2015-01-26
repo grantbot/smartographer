c10857263aaaf54bf5c523f4f192787be4c60c27
Grant Wu <grantwu2@gmail.com>
2015-01-25 21:47:35 -0800

    Implement basic map

--- static/app.js
+++ static/app.js
@@ -1,1 +1,21 @@
-console.log('IM SOME JAVASCRIPT');
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('http://{s}.tiles.mapbox.com/v3/grantbot.kjfg3cap/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18
+    }).addTo(map);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+  console.log(e);
+  popup
+    .setLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(map);
+}
+
+map.on('click', onMapClick);
+
+
+
