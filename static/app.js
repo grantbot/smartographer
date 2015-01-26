c54a22dea12c4e5711dec4d9a0c29a396f48b618
Grant Wu <grantwu2@gmail.com>
2015-01-26 00:29:15 -0800

    (feat) Implement basic heat map

--- static/app.js
+++ static/app.js
@@ -6,4 +6,26 @@ L.tileLayer('http://{s}.tiles.mapbox.com/v3/grantbot.kjfg3cap/{z}/{x}/{y}.png', 
     maxZoom: 18
     }).addTo(map);
 
-omnivore.kml('static/history-12-31-1969.kml').addTo(map);
+var geoJson;
+$.ajax('static/history-12-31-1969.kml').done(function(kml) {
+  geoJson = toGeoJSON.kml(kml);
+  console.log(geoJson.features[0]);
+  geoHeat = geoJson2heat(geoJson, 2);
+
+  var heat = L.heatLayer(
+      geoHeat, {
+        radius: 10,
+        blur: 1, 
+        maxZoom: 17,
+  }).addTo(map);
+});
+
+function geoJson2heat(geojson, intensity) {
+  return geojson.features[0].geometry.coordinates.map(function(feature) {
+    return [
+      feature[1],
+      feature[0],
+      intensity
+    ];
+  });
+}
